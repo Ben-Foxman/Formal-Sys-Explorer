3bbecfa17ef4ed5830943b5ec08ebf6663bc20e9
Ben-Foxman <ben.foxman@yale.edu>
2020-12-23 16:53:37 -0500

    began working on interface

--- fsys.py
+++ fsys.py
@@ -5,19 +5,22 @@ from rules import RuleManager
 
 class FSys:
     def __init__(self):
-        self.alphabet, self.axioms, self.rules, self.depth = self.get_setup()
-        print(self)
+        self.alphabet, self.axioms, self.rules = self.get_setup()
+        self.manage_interface()
 
     def __repr__(self):
         return colored("--System Description--\n", "cyan", attrs=['bold']) + "Alphabet: {}\nAxioms: {}\nInference Rules: \n{}"\
             .format("".join(self.alphabet), ",".join(self.axioms), self.rules) +\
                colored("--End of Description--", "cyan", attrs=['bold'])
+
+    @staticmethod
+    def error_msg(msg):
+        print(colored("FSys:", "red", attrs=['bold']), colored(msg, "white", attrs=['bold']))
     """
     Setup: 
     alphabet -> s.x where x is string containing characters allowed. alphabet must be the first argument
     axiom -> a.x, where x is an axiom
     rule -> r.x, where x is an inference rule of type Rule 
-    depth d.x, where x > 0
     Last always takes precedence, all axioms checked against alphabet at the end
     
     Defaults: 
@@ -27,11 +30,11 @@ class FSys:
     depth = 5
     """
     def get_setup(self):
-        s, a, r, d = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ0123456789-"), [], RuleManager(), 5
+        s, a, r = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ0123456789-"), [], RuleManager()
         for arg in sys.argv[1:]:
-            if len(arg) < 2 or arg[0] not in ['a', 'r', 'd', 's'] or arg[1] != '.' or \
+            if len(arg) < 2 or arg[0] not in ['a', 'r', 's'] or arg[1] != '.' or \
                     (arg[0] == 's' and sys.argv.index(arg) != 1):
-                print("Usage: [s.x][a.x | r.x | d.x]")
+                print("Usage: [s.x][a.x | r.x]")
                 exit(1)
 
             info = arg[2:]
@@ -42,14 +45,8 @@ class FSys:
                 r.set_alphabet(s)
             elif arg[0] == 'a':
                 a.append(info)
-            elif arg[0] == 'r':
-                r.add_rule(info)
             else:
-                try:
-                    d = int("".join(info))
-                except:
-                    self.error_msg("{}: invalid depth".format(info))
-                    exit(1)
+                r.add_rule(info)
         if len(a) == 0:
             a.append("".join(s))
         # make sure axioms fit alphabet
@@ -59,11 +56,24 @@ class FSys:
                     self.error_msg("Axiom '{}' consists of characters not in alphabet.".format(ax))
                     a.remove(ax)
                     break
-        return s, a, r, d
 
-    @staticmethod
-    def error_msg(msg):
-        print(colored("FSys:", "red", attrs=['bold']), colored(msg, "white", attrs=['bold']))
+        return s, a, r
+
+    def manage_interface(self):
+        while True:
+            cmd = input(colored("Enter a command. Type \"help\" for a list of commands.\n", attrs=['bold']))
+            if cmd == "help":
+                with open("help.docs", "r") as file:
+                    print("".join(file.readlines()))
+            elif cmd == "exit":
+                exit(0)
+            elif cmd == "print":
+                print(self)
+            else:
+                self.error_msg("Invalid command: {}".format(cmd))
+
+
+
 
 
 f = FSys()
